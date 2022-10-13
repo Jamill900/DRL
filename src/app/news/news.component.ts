c61f0f1f32d5d602467b146c637c40ed002f6ff2
jamill900 <jamil.aliyev900@outlook.com>
2022-10-13 17:02:00 +0400

    updates

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DatabaseService } from '../database.service';
+import { News } from '../interfaces/news';
 
 @Component({
   selector: 'app-news',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsComponent implements OnInit {
 
-  constructor() { }
+  news: News[] = [];
+  loading = false;
+  p: number = 1;
+
+  constructor(private database: DatabaseService) { }
 
   ngOnInit(): void {
+    this.getNews();
+  }
+
+  getNews() {
+    this.database.getNews().subscribe(news => 
+      this.news = news.reverse());
   }
 
 }
