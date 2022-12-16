073dbb8f74182f15cccbde13351a874a8ae4d8dc
Jamil Alizada <xalizadax@gmail.com>
2022-12-16 14:55:38 +0400

    Drl Videos Update

--- src/app/aiqum/aiqum.component.ts
+++ src/app/aiqum/aiqum.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgbCarousel, NgbModal, NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/ng-bootstrap';
 import * as AOS from 'aos';
 
 @Component({
@@ -15,12 +15,44 @@ export class AiqumComponent implements OnInit {
     AOS.init();
   }
 
+  images = [1, 2, 3, 4].map((n) => `assets/products/AiQum_${n}.jpg`);
+
+	paused = false;
+	unpauseOnArrow = false;
+	pauseOnIndicator = false;
+	pauseOnHover = true;
+	pauseOnFocus = true;
+
+  @ViewChild('carousel', { static: true }) carousel: NgbCarousel;
+
+	togglePaused() {
+		if (this.paused) {
+			this.carousel.cycle();
+		} else {
+			this.carousel.pause();
+		}
+		this.paused = !this.paused;
+	}
+
   scrollToElement(element: any): void {
     element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
   }
 
   openLg(content: any) {
 		this.modalService.open(content, { size: 'lg' });
+	}
+
+  onSlide(slideEvent: NgbSlideEvent) {
+		if (
+			this.unpauseOnArrow &&
+			slideEvent.paused &&
+			(slideEvent.source === NgbSlideEventSource.ARROW_LEFT || slideEvent.source === NgbSlideEventSource.ARROW_RIGHT)
+		) {
+			this.togglePaused();
+		}
+		if (this.pauseOnIndicator && !slideEvent.paused && slideEvent.source === NgbSlideEventSource.INDICATOR) {
+			this.togglePaused();
+		}
 	}
 
 }
