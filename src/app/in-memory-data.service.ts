6da26801a198f68003913b70d61d11068b276db2
Jamil Alizada <xalizadax@gmail.com>
2023-05-11 17:03:48 +0400

    Careers Update

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -9,12 +9,6 @@ export class InMemoryDataService implements InMemoryDbService {
     let careers = [
       {
         id: 0,
-        name: 'Senior Python\nDeveloper',
-        location: 'Baku, Azerbaijan',
-        link: 'https://forms.clickup.com/31136212/f/xp6em-2263/IMXCVX7064QS24PMVQ'
-      },
-      {
-        id: 1,
         name: 'Graduate Petroleum\nEngineer',
         location: 'Baku, Azerbaijan',
         link: 'https://forms.clickup.com/31136212/f/xp6em-2423/A6MKNTETY4YJOYGXNJ'
